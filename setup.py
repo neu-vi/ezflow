0d9d5978110875a826d162a2166fa5492a2c63f0
NeelayS <shahnh19@gmail.com>
2021-08-26 23:24:08 +0530

    Removed long description from README

--- setup.py
+++ setup.py
@@ -7,13 +7,9 @@ import os
 
 from setuptools import find_packages, setup
 
-with open("./README.md") as readme_file:
-    readme = readme_file.read()
-
-
 PROJECT = os.path.abspath(os.path.dirname(__file__))
-REQUIRE_PATH = "./requirements.txt"
-PKG_DESCRIBE = "./README.md"
+REQUIRE_PATH = "requirements.txt"
+PKG_DESCRIBE = "README.md"
 
 
 def read(*parts):
@@ -63,7 +59,6 @@ setup(
     },
     install_requires=list(get_requires()),
     license="MIT license",
-    long_description=readme,
     include_package_data=True,
     keywords="openoptflow",
     name="openoptflow",
