dec6cff00765cd36c08ae295e2349d17583b568d
NeelayS <shahnh19@gmail.com>
2021-08-27 21:29:22 +0530

    Added base trainer class

--- openoptflow/utils/other_utils.py
+++ openoptflow/utils/other_utils.py
@@ -5,3 +5,21 @@ def coords_grid(batch_size, h, w):
     coords = torch.meshgrid(torch.arange(h), torch.arange(w))
     coords = torch.stack(coords[::-1], dim=0).float()
     return coords[None].repeat(batch_size, 1, 1, 1)
+
+
+class AverageMeter:
+    def __init__(self):
+
+        self.reset()
+
+    def reset(self):
+        self.val = 0.0
+        self.avg = 0.0
+        self.sum = 0.0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
