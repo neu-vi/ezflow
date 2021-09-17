faf3354a13f8ce700b4326aa25064100365b53f6
Prajnan Goswami <prajnan.videoworks@gmail.com>
2021-09-17 00:02:59 -0400

    Added BaseDataset

---
+++ openoptflow/dataset/base_dataset.py
@@ -0,0 +1,65 @@
+import random
+
+import numpy as np
+import torch
+import torch.utils.data as data
+
+from openoptflow.dataset import frame_utils
+
+
+class BaseDataset(data.Dataset):
+    def __init__(self):
+        self.augmentor = None
+        self.isTest = False
+        self.init_seed = False
+        self.flow_list = []
+        self.image_list = []
+
+    def __getitem__(self, index):
+        if self.is_test:
+            img1 = frame_utils.read_gen(self.image_list[index][0])
+            img2 = frame_utils.read_gen(self.image_list[index][1])
+            img1 = np.array(img1).astype(np.uint8)[..., :3]
+            img2 = np.array(img2).astype(np.uint8)[..., :3]
+            img1 = torch.from_numpy(img1).permute(2, 0, 1).float()
+            img2 = torch.from_numpy(img2).permute(2, 0, 1).float()
+            return img1, img2
+
+        if not self.init_seed:
+            worker_info = torch.utils.data.get_worker_info()
+            if worker_info is not None:
+                torch.manual_seed(worker_info.id)
+                np.random.seed(worker_info.id)
+                random.seed(worker_info.id)
+                self.init_seed = True
+
+        index = index % len(self.image_list)
+
+        img1 = frame_utils.read_gen(self.image_list[index][0])
+        img2 = frame_utils.read_gen(self.image_list[index][1])
+        flow = frame_utils.read_gen(self.flow_list[index])
+
+        flow = np.array(flow).astype(np.float32)
+        img1 = np.array(img1).astype(np.uint8)
+        img2 = np.array(img2).astype(np.uint8)
+
+        # grayscale images
+        if len(img1.shape) == 2:
+            img1 = np.tile(img1[..., None], (1, 1, 3))
+            img2 = np.tile(img2[..., None], (1, 1, 3))
+        else:
+            img1 = img1[..., :3]
+            img2 = img2[..., :3]
+
+        if self.augmentor is not None:
+            img1, img2, flow = self.augmentor(img1, img2, flow)
+
+        return img1, img2, flow
+
+    def __rmul__(self, v):
+        self.flow_list = v * self.flow_list
+        self.image_list = v * self.image_list
+        return self
+
+    def __len__(self):
+        return len(self.image_list)
