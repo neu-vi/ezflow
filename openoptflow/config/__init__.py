4321942e2b993de70ef95512d263909a6158a5e6
NeelayS <shahnh19@gmail.com>
2021-09-21 21:27:35 +0530

    Create config parsers

---
+++ openoptflow/config/__init__.py
@@ -0,0 +1,1 @@
+from .config import CfgNode, configurable, get_default_cfg
--- openoptflow/models/model_zoo.py
+++ openoptflow/models/model_zoo.py
@@ -6,6 +6,8 @@ import os
 
 import pkg_resources
 
+from ..config import get_default_cfg
+
 
 def get_cfg_file(cfg_path):
 
@@ -49,44 +51,7 @@ def get_cfg(cfg_path):
 
     cfg_file = get_cfg_file(cfg_path)
 
-    cfg = CfgNode  # should be a empty CfgNode or a default
+    cfg = get_default_cfg()
     cfg.merge_from_file(cfg_file)
 
     return cfg
-
-
-def get(cfg_path, trained: bool = False, device: Optional[str] = None):
-    """
-    Get a model specified by relative path under Detectron2's official ``configs/`` directory.
-
-    Args:
-        config_path (str): config file name relative to detectron2's "configs/"
-            directory, e.g., "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml"
-        trained (bool): see :func:`get_config`.
-        device (str or None): overwrite the device in config, if given.
-
-    Returns:
-        nn.Module: a detectron2 model. Will be in training mode.
-
-    Example:
-    ::
-        from detectron2 import model_zoo
-        model = model_zoo.get("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml", trained=True)
-    """
-    cfg = get_cfg(cfg_path, trained)
-
-    if device is None and not torch.cuda.is_available():
-        device = "cpu"
-    if device is not None and isinstance(cfg, CfgNode):
-        cfg.MODEL.DEVICE = device
-
-    if isinstance(cfg, CfgNode):
-        model = build_model(cfg)
-        DetectionCheckpointer(model).load(cfg.MODEL.WEIGHTS)
-    else:
-        model = instantiate(cfg.model)
-        if device is not None:
-            model = model.to(device)
-        if "train" in cfg and "init_checkpoint" in cfg.train:
-            DetectionCheckpointer(model).load(cfg.train.init_checkpoint)
-    return model
