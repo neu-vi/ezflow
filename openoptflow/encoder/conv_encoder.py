3bd94a854ac88f09b58811fd92b0a36ee877cb86
Prajnan Goswami <prajnan.videoworks@gmail.com>
2021-11-11 23:56:50 -0500

    [WIP]added conv encoder

--- openoptflow/encoder/conv_encoder.py
+++ openoptflow/encoder/conv_encoder.py
@@ -4,18 +4,34 @@ import torch.nn as nn
 from ..config import configurable
 from .build import ENCODER_REGISTRY
 
+
 def conv(in_planes, out_planes, kernel_size=3, stride=1, norm=None):
     if norm.lower() == "batch":
         return nn.Sequential(
-            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, bias=False),
+            nn.Conv2d(
+                in_planes,
+                out_planes,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=(kernel_size - 1) // 2,
+                bias=False,
+            ),
             nn.BatchNorm2d(out_planes),
-            nn.LeakyReLU(0.1,inplace=True)
+            nn.LeakyReLU(0.1, inplace=True),
         )
     else:
         return nn.Sequential(
-            nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=(kernel_size-1)//2, bias=True),
-            nn.LeakyReLU(0.1,inplace=True)
+            nn.Conv2d(
+                in_planes,
+                out_planes,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=(kernel_size - 1) // 2,
+                bias=True,
+            ),
+            nn.LeakyReLU(0.1, inplace=True),
         )
+
 
 @ENCODER_REGISTRY.register()
 class ConvEncoder(nn.Module):
@@ -23,18 +39,24 @@ class ConvEncoder(nn.Module):
 
     @configurable
     def __init__(
-        self, 
-        in_channels=3, 
+        self,
+        in_channels=3,
         channels=[64, 128, 256],
         kernels=[3, 3, 3],
         strides=[1, 1, 1],
-        norm=None
+        norm=None,
     ):
         super(ConvEncoder, self).__init__()
 
-        assert len(channels)==len(kernels), "number of kernels and channels are not equal"
-        assert len(channels)==len(strides), "number of strides and channels are not equal"
-        assert norm.lower()=="batch" or norm==None, f"{norm} not supported in conv encoder" 
+        assert len(channels) == len(
+            kernels
+        ), "number of kernels and channels are not equal"
+        assert len(channels) == len(
+            strides
+        ), "number of strides and channels are not equal"
+        assert (
+            norm.lower() == "batch" or norm == None
+        ), f"{norm} not supported in conv encoder"
 
         norm = "batch" if norm.lower() == "batch" else None
 
@@ -44,17 +66,22 @@ class ConvEncoder(nn.Module):
             kernels = list(kernels)
         if isinstance(strides, tuple):
             strides = list(strides)
-        
+
         channels = [in_channels] + config
 
         self.encoder = nn.ModuleList()
 
-        for i in range(len(config)-1):
+        for i in range(len(config) - 1):
             self.encoder.append(
-                conv(config[i], config[i+1], kernel_size=kernels[i], stride=strides[i],norm=norm)
+                conv(
+                    config[i],
+                    config[i + 1],
+                    kernel_size=kernels[i],
+                    stride=strides[i],
+                    norm=norm,
+                )
             )
 
-    
     @classmethod
     def from_config(self, cfg):
         return {
@@ -62,12 +89,12 @@ class ConvEncoder(nn.Module):
             "channels": cfg.LAYER_CONFIG.CHANNELS,
             "kernels": cfg.LAYER_CONFIG.KERNELS,
             "strides": cfg.LAYER_CONFIG.STRIDES,
-            "NORM": cfg.NORM
+            "NORM": cfg.NORM,
         }
 
     def forward(self, x):
-        
+
         for i in range(len(self.encoder)):
             x = self.encoder[i](x)
 
-        return x 
+        return x
