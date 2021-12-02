c9dfb05121def36f4ebf8177f88ccf01a8b79660
NeelayS <shahnh19@gmail.com>
2021-12-02 18:35:32 +0530

    1. Added test for FlowNetConvDecoder 2. Made some formatting changes

--- ezflow/decoder/__init__.py
+++ ezflow/decoder/__init__.py
@@ -1,5 +1,5 @@
 from .build import DECODER_REGISTRY, build_decoder
-from .conv_decoder import ConvDecoder, FlownetConvDecoder
+from .conv_decoder import ConvDecoder, FlowNetConvDecoder
 from .iterative import *
 from .noniterative import *
 from .separable_conv import Butterfly4D, SeparableConv4D
