439beced11c98559528b08852654ee98a08b6cd1
NeelayS <shahnh19@gmail.com>
2021-12-02 13:52:41 +0530

    Created files for pruner

--- ezflow/functional/__init__.py
+++ ezflow/functional/__init__.py
@@ -1,4 +1,5 @@
 from .criterion import *
 from .data_augmentation import *
+from .pruning import prune_model
 from .registry import FUNCTIONAL_REGISTRY, get_functional
 from .scheduler import CosineWarmupScheduler
---
+++ ezflow/functional/pruning.py
@@ -0,0 +1,5 @@
+import torch
+
+
+def prune_model():
+    pass
