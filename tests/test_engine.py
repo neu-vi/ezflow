2b8063b155d5f2fd74c264dc4e210372b96faddf
NeelayS <shahnh19@gmail.com>
2021-12-05 12:03:12 +0530

    Added dummy test for pruning

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,1 +1,14 @@
 import torch
+from torch import nn
+
+from ezflow.engine import prune_model
+
+
+def test_pruning():
+
+    model = nn.Sequential(
+        nn.Linear(32, 16),
+        nn.Linear(16, 8),
+        nn.Linear(8, 4),
+    )
+    _ = prune_model(model, 0.5)
