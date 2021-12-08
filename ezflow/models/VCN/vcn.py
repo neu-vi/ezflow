fcd88bb9a9d1b2e86bfa56b903417c7947830d99
prajnan93 <prajnan.videoworks@gmail.com>
2021-12-07 23:08:25 -0500

    [WIP] removing print statements

--- ezflow/models/VCN/vcn.py
+++ ezflow/models/VCN/vcn.py
@@ -261,22 +261,12 @@ class VCN(nn.Module):
             )
             cost = self.butterfly_filters[i](cost)
             cost = self.sep_conv_4d_filters[i](cost)
-            print(" cost: ", cost.shape)
+
             B, C, U, V, H, W = cost.shape
             cost = cost.view(-1, U, V, H, W)
             flow, ent = self.flow_regressors[i](cost)
 
             if i != 0:
-                print(
-                    "step: ",
-                    i,
-                    " flow: ",
-                    flow.shape,
-                    "ent: ",
-                    ent.shape,
-                    " up_flow: ",
-                    up_flow.shape,
-                )
                 flow = flow.view(B, C, 2, H, W) + up_flow[:, np.newaxis]
 
             flow = flow.view(batch_size, -1, H, W)
