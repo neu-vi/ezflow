34f43bdd4418ea1592699009ac9e8786aa856709
prajnan93 <prajnan.videoworks@gmail.com>
2021-12-26 19:59:00 -0500

    [WIP] added sparse augmentor

--- ezflow/functional/data_augmentation/augmentor.py
+++ ezflow/functional/data_augmentation/augmentor.py
@@ -66,3 +66,61 @@ class FlowAugmentor:
         flow = np.ascontiguousarray(flow)
 
         return img1, img2, flow
+
+
+@FUNCTIONAL_REGISTRY.register()
+class SparseFlowAugmentor(FlowAugmentor):
+    """
+    Class for appyling a series of augmentations to a pair of images and a flow field
+
+    Parameters
+    ----------
+    crop_size : int
+        Size of the crop to be applied to the images.
+    color_aug_params : dict
+        Parameters for the color augmentation.
+    eraser_aug_params : dict
+        Parameters for the eraser augmentation.
+    spatial_aug_params : dict
+        Parameters for the spatial augmentation.
+    """
+
+    def __call__(self, img1, img2, flow, valid):
+        """
+        Applies the augmentations to the pair of images and the flow field.
+
+        Parameters
+        ----------
+        img1 : numpy.ndarray
+            First image
+        img2 : numpy.ndarray
+            Second image
+        flow : numpy.ndarray
+            Flow field
+        valid : numpy.ndarray
+            Valid Flow field
+
+        Returns
+        -------
+        img1 : numpy.ndarray
+            First image
+        img2 : numpy.ndarray
+            Second image
+        flow : numpy.ndarray
+            Flow field
+        valid : numpy.ndarray
+            Valid Flow field
+        """
+
+        img1, img2 = color_transform(img1, img2, **self.color_aug_params)
+        img1, img2 = eraser_transform(img1, img2, **self.eraser_aug_params)
+        img1, img2, flow, valid = sparse_spatial_transform(
+            img1, img2, flow, valid, self.crop_size, **self.spatial_aug_params
+        )
+
+        img1 = np.ascontiguousarray(img1)
+        img2 = np.ascontiguousarray(img2)
+        flow = np.ascontiguousarray(flow)
+        valid = np.ascontiguousarray(valid)
+
+        return img1, img2, flow, valid
--- ezflow/functional/data_augmentation/operations.py
+++ ezflow/functional/data_augmentation/operations.py
@@ -193,3 +193,93 @@ def spatial_transform(
     flow = flow[y0 : y0 + crop_size[0], x0 : x0 + crop_size[1]]
 
     return img1, img2, flow
+
+
+def resize_sparse_flow_map(self, flow, valid, fx=1.0, fy=1.0):
+    """"""
+    ht, wd = flow.shape[:2]
+    coords = np.meshgrid(np.arange(wd), np.arange(ht))
+    coords = np.stack(coords, axis=-1)
+
+    coords = coords.reshape(-1, 2).astype(np.float32)
+    flow = flow.reshape(-1, 2).astype(np.float32)
+    valid = valid.reshape(-1).astype(np.float32)
+
+    coords0 = coords[valid >= 1]
+    flow0 = flow[valid >= 1]
+
+    ht1 = int(round(ht * fy))
+    wd1 = int(round(wd * fx))
+
+    coords1 = coords0 * [fx, fy]
+    flow1 = flow0 * [fx, fy]
+
+    xx = np.round(coords1[:, 0]).astype(np.int32)
+    yy = np.round(coords1[:, 1]).astype(np.int32)
+
+    v = (xx > 0) & (xx < wd1) & (yy > 0) & (yy < ht1)
+    xx = xx[v]
+    yy = yy[v]
+    flow1 = flow1[v]
+
+    flow_img = np.zeros([ht1, wd1, 2], dtype=np.float32)
+    valid_img = np.zeros([ht1, wd1], dtype=np.int32)
+
+    flow_img[yy, xx] = flow1
+    valid_img[yy, xx] = 1
+
+    return flow_img, valid_img
+
+
+def sparse_spatial_transform(
+    img1,
+    img2,
+    flow,
+    valid,
+    crop_size,
+    aug_prob=0.8,
+    min_scale=-0.2,
+    max_scale=0.5,
+    flip=True,
+    h_flip_prob=0.5,
+):
+
+    H, W = img1.shape[:2]
+    min_scale = np.maximum((crop_size[0] + 1) / float(H), (crop_size[1] + 1) / float(W))
+
+    scale = 2 ** np.random.uniform(min_scale, max_scale)
+    scale_x = np.clip(scale_x, min_scale, None)
+    scale_y = np.clip(scale_y, min_scale, None)
+
+    if np.random.rand() < aug_prob:
+
+        img1 = cv2.resize(
+            img1, None, fx=scale_x, fy=scale_y, interpolation=cv2.INTER_LINEAR
+        )
+        img2 = cv2.resize(
+            img2, None, fx=scale_x, fy=scale_y, interpolation=cv2.INTER_LINEAR
+        )
+        flow, valid = self.resize_sparse_flow_map(flow, valid, fx=scale_x, fy=scale_y)
+
+    if flip:
+        if np.random.rand() < h_flip_prob:
+            img1 = img1[:, ::-1]
+            img2 = img2[:, ::-1]
+            flow = flow[:, ::-1] * [-1.0, 1.0]
+            valid = valid[:, ::-1]
+
+    margin_y = 20
+    margin_x = 50
+
+    y0 = np.random.randint(0, img1.shape[0] - crop_size[0] + margin_y)
+    x0 = np.random.randint(-margin_x, img1.shape[1] - crop_size[1] + margin_x)
+
+    y0 = np.clip(y0, 0, img1.shape[0] - crop_size[0])
+    x0 = np.clip(x0, 0, img1.shape[1] - crop_size[1])
+
+    img1 = img1[y0 : y0 + crop_size[0], x0 : x0 + crop_size[1]]
+    img2 = img2[y0 : y0 + crop_size[0], x0 : x0 + crop_size[1]]
+    flow = flow[y0 : y0 + crop_size[0], x0 : x0 + crop_size[1]]
+    valid = valid[y0 : y0 + crop_size[0], x0 : x0 + crop_size[1]]
+
+    return img1, img2, flow, valid
