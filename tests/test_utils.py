577bdb2b1b2406eec7d6ce0733bdaa4d2df9a928
NeelayS <shahnh19@gmail.com>
2022-01-11 16:39:55 +0530

    Formatting

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,11 +7,9 @@ def test_endpointerror():
 
     pred = torch.rand(4, 2, 256, 256)
     target = torch.rand(4, 2, 256, 256)
-
     _ = endpointerror(pred, target)
 
     target = torch.rand(
         4, 3, 256, 256
     )  # Ignore valid mask for EPE calculation if target contains it
-
     _ = endpointerror(pred, target)
