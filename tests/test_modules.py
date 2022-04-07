1b4031121f685cb866d22f01927d34f4cbfb64c4
NeelayS <shahnh19@gmail.com>
2022-04-07 13:04:07 +0530

    Increased code coverage for modules/

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -12,3 +12,84 @@ def test_ConvGRU():
 
     module = MODULE_REGISTRY.get("ConvGRU")(hidden_dim=8, input_dim=8)
     _ = module(inp_h, inp_x)
+
+
+def test_BasicBlock():
+
+    inp = torch.randn(2, 3, 256, 256)
+
+    module = MODULE_REGISTRY.get("BasicBlock")(
+        inp.shape[1], 32, norm="group", activation="relu", stride=3
+    )
+    _ = module(inp)
+    del module
+
+    module = MODULE_REGISTRY.get("BasicBlock")(
+        inp.shape[1], 32, norm="batch", activation="leakyrelu", stride=3
+    )
+    _ = module(inp)
+    del module
+
+    module = MODULE_REGISTRY.get("BasicBlock")(
+        inp.shape[1], 32, norm="instance", activation="relu", stride=3
+    )
+    _ = module(inp)
+    del module
+
+    module = MODULE_REGISTRY.get("BasicBlock")(
+        inp.shape[1], 32, norm="none", activation="relu", stride=3
+    )
+    _ = module(inp)
+    del module
+
+    module = MODULE_REGISTRY.get("BasicBlock")(
+        inp.shape[1], 32, norm=None, activation="relu", stride=3
+    )
+    _ = module(inp)
+    del module
+
+
+def test_BottleneckBlock():
+
+    inp = torch.randn(2, 3, 256, 256)
+
+    module = MODULE_REGISTRY.get("BottleneckBlock")(
+        inp.shape[1], 32, norm="group", activation="relu", stride=3
+    )
+    _ = module(inp)
+    del module
+
+    module = MODULE_REGISTRY.get("BottleneckBlock")(
+        inp.shape[1], 32, norm="batch", activation="leakyrelu", stride=3
+    )
+    _ = module(inp)
+    del module
+
+    module = MODULE_REGISTRY.get("BottleneckBlock")(
+        inp.shape[1], 32, norm="instance", activation="relu", stride=3
+    )
+    _ = module(inp)
+    del module
+
+    module = MODULE_REGISTRY.get("BottleneckBlock")(
+        inp.shape[1], 32, norm="none", activation="relu", stride=3
+    )
+    _ = module(inp)
+    del module
+
+    module = MODULE_REGISTRY.get("BottleneckBlock")(
+        inp.shape[1], 32, norm=None, activation="relu", stride=3
+    )
+    _ = module(inp)
+    del module
+
+
+def test_DAP():
+
+    inp = torch.randn(2, 1, 7, 7, 16, 16)
+
+    module = MODULE_REGISTRY.get("DisplacementAwareProjection")(temperature=False)
+    _ = module(inp)
+
+    module = MODULE_REGISTRY.get("DisplacementAwareProjection")(temperature=True)
+    _ = module(inp)
