e5d888322be8b49c8eeb89e1d7ccef04a70fa74a
NeelayS <shahnh19@gmail.com>
2022-04-09 23:38:01 +0530

    Fixed path issue

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,15 +1,14 @@
 from torch import nn
 from torch.utils.data import DataLoader
 
-
 from ezflow.engine import (
     Trainer,
     get_training_cfg,
     prune_l1_structured,
     prune_l1_unstructured,
 )
-from .utils import MockOpticalFlowDataset, MockOpticalFlowModel
 
+from .utils import MockOpticalFlowDataset, MockOpticalFlowModel
 
 img_size = (64, 64)
 img_channels = 3
@@ -26,14 +25,14 @@ mock_model = MockOpticalFlowModel(img_channels=img_channels)
 def test_Trainer():
 
     training_cfg = get_training_cfg(
-        cfg_path="configs/base_trainer_test.yaml", custom=True
+        cfg_path="./configs/base_trainer_test.yaml", custom=True
     )
     train_loader = val_loader = mock_dataloader
 
     trainer = Trainer(training_cfg, mock_model, train_loader, val_loader)
     trainer.train(n_epochs=1)
     trainer.resume_training(
-        consolidated_ckpt=f"ckpts/{mock_model.__class__.__name__}_epochs1.pth",
+        consolidated_ckpt=f"./ckpts/{mock_model.__class__.__name__}_epochs1.pth",
         n_epochs=1,
     )
 
