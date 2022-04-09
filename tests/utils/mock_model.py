cb4a5ccbd2d388e0c7e9f8beae28fb4626726ba7
NeelayS <shahnh19@gmail.com>
2022-04-10 00:06:20 +0530

    Fixed mock model

--- tests/utils/mock_model.py
+++ tests/utils/mock_model.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 
@@ -5,7 +6,10 @@ class MockOpticalFlowModel(nn.Module):
     def __init__(self, img_channels):
         super().__init__()
 
-        self.model = nn.Conv2d(img_channels, 2, kernel_size=1)
+        self.model = nn.Conv2d(img_channels * 2, 2, kernel_size=1)
 
-    def forward(self, x):
+    def forward(self, img1, img2):
+
+        x = torch.cat([img1, img2], dim=-3)
+
         return self.model(x)
