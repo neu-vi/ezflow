b7721f6297a3fb5e832e6caee8c40fe07264719d
Prajnan Goswami <prajnan.videoworks@gmail.com>
2022-04-18 16:50:36 -0400

    updated models with BaseModule

--- ezflow/models/dicl.py
+++ ezflow/models/dicl.py
@@ -4,14 +4,14 @@ import torch.nn.functional as F
 
 from ..decoder import FlowEntropy, build_decoder
 from ..encoder import build_encoder
-from ..modules import ConvNormRelu, build_module
+from ..modules import BaseModule, ConvNormRelu, build_module
 from ..similarity import build_similarity
 from ..utils import warp
 from .build import MODEL_REGISTRY
 
 
 @MODEL_REGISTRY.register()
-class DICL(nn.Module):
+class DICL(BaseModule):
     """
     Implementation of the paper
     `Displacement-Invariant Matching Cost Learning for Accurate Optical Flow Estimation <https://arxiv.org/abs/2010.14851>`_
